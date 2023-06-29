23e52890e62ad5a16baba1bd8f3c6ce86ba8a1fa
megha2601 <109163494+megha2601@users.noreply.github.com>
2023-06-29 13:20:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import RegForm from "./components/RegForm";
+
+function App() {
+  return (
+    <div id="system">
+      <RegForm />
+    </div>
+  );
+}
+
+export default App;
